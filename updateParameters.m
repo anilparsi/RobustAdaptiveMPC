de16444a4e3fdaebced146dbcbd9554937ec1a2d
anilparsi <anilparc@gmail.com>
2020-03-04 21:13:20 +0100

    Update updateParameters.m
    Changed update of parameters to h_theta_k instead of h_theta_0

--- updateParameters.m
+++ updateParameters.m
@@ -43,14 +43,14 @@ cont.h_theta_k = h;
 %% Estimate of theta
 theta_til = cont.theta_hat + cont.mu * Dk(end-sys.n+1:end,:)'*(xk-cont.x_hat_k); 
 
-if any(cont.H_theta*theta_til>=cont.h_theta_0)
+if any(cont.H_theta*theta_til>=cont.h_theta_k)
     % theta_til outside the initial bounds, use projection
     cvx_begin 
         variable theta_hat_p(sys.p,1)
         minimize sum((theta_hat_p-theta_til).^2)
         
         subject to
-            cont.H_theta*theta_hat_p <= cont.h_theta_0;
+            cont.H_theta*theta_hat_p <= cont.h_theta_k;
         
     cvx_end
     
